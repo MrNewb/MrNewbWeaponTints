c092e319c6470e0e8bbfcc7c6deacd5a9a100749
Donivan <donivansee@live.com>
2023-10-19 01:31:31 -0400

    move to consume

--- Config.lua
+++ Config.lua
@@ -1,6 +1,4 @@
 Config = {}
 Config.notificationtitle = 'Weapon Modification'	--- title notification change to whatever
 Config.scratchedserial = "The serial has been scratched off."  -- Can place whatever you want here, change it to 0000 or 656565656565----- or whatever you want
-Config.breakpoint = 9  --- number to hit from breakpoint greater than or equal to chancerandom
-Config.chancerandom = math.random(1,10)	--- math random min, max numbers to roll for break chance
 Config.debugprints = true	-- do you want strings printing in the console to debug?
